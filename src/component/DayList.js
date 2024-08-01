3eb7deb4454eb5fa48609f4540b864d3359b9e48
boby <puddingbeee@gmail.com>
2024-08-01 15:13:46 +0900

    Back up

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,19 +1,23 @@
-import dummy from "../db/data.json";       // 여기서는 {}로 안 불러오네. 왜지?
-//export 'dummy' (imported as 'dummy') was not found in '../db/data.json' (possible exports: days, words)
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export const DayList = () => {
-    console.log(dummy);
+    const [days, setDays] = useState([]); // 처음에는 빈 배열을 가져와서 API에서 리스트 가져오기
+
+    useEffect(() => { // useEffect 목적: 렌더링이 되고 [API를 호출]한다!
+        console.log("Count change")
+    }, []) // 빈 배열을 넣으면 (상태값 변경과 무관하게) 렌더링이 완료되고 최초에 한 번만 실행된다.
+
     return(
-        <ul className="list_day">
-            {
-                dummy.days.map(day => ( // dummy데이터의 days키의 배열을 가지고 와서 반복한다.
-                                        //key는 반복되는 요소에 고유한 값을 넣어주어야 한다.
-                    <li key={day.id}>   
-                        <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-                    </li>
-                ))
-            }
-        </ul>
+            <ul className="list_day">
+                {
+                    days.map(day => ( // dummy데이터의 days키의 배열을 가지고 와서 반복한다.
+                                            //key는 반복되는 요소에 고유한 값을 넣어주어야 한다.
+                        <li key={day.id}>   
+                            <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+                        </li>
+                    ))
+                }
+            </ul>
     )
 }
