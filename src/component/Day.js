bce95b602086e59eda7e1623ffb8beb40f0e6e71
boby <puddingbeee@gmail.com>
2024-08-01 16:29:36 +0900

    useEffect와 fetch로 API 연결하여 json data 가져오기

--- src/component/Day.js
+++ src/component/Day.js
@@ -4,22 +4,24 @@ import { useEffect, useState } from "react";
 
 export const Day = () => {
 
-    const day = useParams(); //useParam()을 통해서 주솔창의 문자열이 들어온다. .day는 뗌
+    const day = useParams().day; 
+    //useParam()을 통해서 주솔창의 문자열이 들어온다. 아 이거 .day는 꼭 붙여야하네 = =
     // const { day } = useParams();
 
     const [words, setWords] = useState([]); // 어? 왜 빈 배열로 만들지?
 
     useEffect(() => {
         // API 비동기 통신을 위해서 fetch 사용
-        fetch(`http://localhost:3002/words?day=${day}`)
+        fetch(`http://localhost:3002/words?day=${day}`) // JSP랑 비슷하구마잉
         .then(res => {
             return res.json();
         })
         .then(data => {
             setWords(data);
         })
-    }, []);
-     
+    }, []); // 빈배열에 day를 넣은 이유> day의 최신값을 보장받을라고.
+               /* 빈배열로 냅두면 day를 최신값으로 유지하지 못하기 때문이다.
+                  이 배열 부분을 의존성 배열이라고 하는 것 같다. */
 
     return (
         <>
